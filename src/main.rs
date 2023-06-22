8c3751212c88be704c5e8500bdc9c402d51f2c48
Jacobtread <jacobtread@gmail.com>
2023-06-22 15:26:02 +1200

    Divided workflow steps, added multiple countries and more information into output file, switch progress println for debug message

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use std::{
     fs::{create_dir_all, read_to_string, File},
     io::Write,
     path::Path,
+    process::exit,
     thread::sleep,
     time::Duration,
 };
 
+use chrono::prelude::*;
 use graphql_client::{reqwest::post_graphql_blocking, GraphQLQuery};
-use log::error;
+use log::{debug, error};
+use preset::LocationPreset;
 use reqwest::{
     blocking::Client,
     header::{HeaderMap, HeaderValue, InvalidHeaderValue, AUTHORIZATION},
@@ -21,14 +24,63 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     let _ = dotenvy::dotenv();
+    env_logger::init();
 
     // Remove blacklisted users
     let blacklist = read_blacklist();
 
     let token = std::env::var("GITHUB_TOKEN").unwrap();
-    let users = search_users(token, &blacklist).unwrap();
 
-    produce_output(users);
+    let mut headers = HeaderMap::with_capacity(1);
+    headers.insert(
+        AUTHORIZATION,
+        HeaderValue::from_str(&format!("Bearer {}", token)).expect("Failed to create token header"),
+    );
+
+    let client = match Client::builder()
+        .user_agent(format!("Committer/{}", VERSION))
+        .default_headers(headers)
+        .build()
+    {
+        Ok(value) => value,
+        Err(error) => {
+            error!("Failed to create request client: {}", error);
+            exit(1);
+        }
+    };
+
+    for preset in preset::PRESETS {
+        let (users, min_followers) = match search_users(&client, &blacklist, preset) {
+            Ok(value) => value,
+            Err(err) => {
+                error!("Failed to complete preset {}: {}", preset.title, err);
+                exit(1);
+            }
+        };
+        if let Err(err) = produce_output(users, preset.title, min_followers) {
+            error!(
+                "Failed to produce preset output for {}: {}",
+                preset.title, err
+            );
+            exit(1);
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Output {
+    title: String,
+    min_followers: i64,
+    generated_at: DateTime<Utc>,
+    users: Vec<User>,
+}
+
+#[derive(Debug, Error)]
+pub enum OutputResult {
+    #[error("Error while serializing results: {0}")]
+    Serialize(#[from] serde_json::Error),
+    #[error("Failed to create/write output file: {0}")]
+    WriteFile(#[from] std::io::Error),
 }
 
 /// Writes the data/ranked.json file with the sorted results
@@ -36,7 +88,11 @@ fn main() {
 ///
 /// # Arguments
 /// * users - The collection of users
-fn produce_output(mut users: Vec<User>) {
+fn produce_output(
+    mut users: Vec<User>,
+    title: &str,
+    min_followers: i64,
+) -> Result<(), OutputResult> {
     let data = Path::new("data");
     if !data.exists() {
         create_dir_all(data).expect("Failed to create data directory");
@@ -45,11 +101,20 @@ fn produce_output(mut users: Vec<User>) {
     // Sort the results by number of commits
     users.sort_by(|a, b| b.commits.cmp(&a.commits));
 
-    let out = data.join("ranked.json");
-    let json: String = serde_json::to_string(&users).expect("Failed to create users JSON");
-    let mut file = File::create(out).expect("Failed to create data/ranked.json");
-    file.write_all(json.as_bytes())
-        .expect("Failed to write ranked.json");
+    let file_name = title.to_lowercase().replace(' ', "+");
+    let out = data.join(format!("{}.json", file_name));
+
+    let output = Output {
+        title: title.to_string(),
+        min_followers,
+        generated_at: Utc::now(),
+        users,
+    };
+
+    let json: String = serde_json::to_string(&output)?;
+    let mut file = File::create(out)?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
 }
 
 /// Reads the collection of blacklisted named from the
@@ -81,8 +146,6 @@ struct UsersQuery;
 /// Errors that could occur while searching for users
 #[derive(Debug, Error)]
 enum SearchError {
-    #[error("Failed to create client: {0}")]
-    CreateClient(reqwest::Error),
     #[error("Invalid token header: {0}")]
     InvalidTokenHeader(#[from] InvalidHeaderValue),
     #[error("Ran out of attempts and failed request")]
@@ -111,44 +174,47 @@ struct User {
 /// Searches for and collects users from GitHub
 ///
 /// # Arguments
-/// * token - The GitHub personal access token
+/// * client - The client to make the graphql requests
 /// * blacklist - List of blacklisted names
-fn search_users(token: String, blacklist: &[Box<str>]) -> Result<Vec<User>, SearchError> {
-    let locations = &preset::PRESET;
-
+/// * location - The location data for the request
+fn search_users(
+    client: &Client,
+    blacklist: &[Box<str>],
+    location: &LocationPreset,
+) -> Result<(Vec<User>, i64), SearchError> {
     /// GitHub API URL for GraphQL
     const GRAPHQL_URL: &str = "https://api.github.com/graphql";
 
     /// The number of users to collect (Considered amount)
     const USERS: usize = 1000;
     /// Number of users to query for each time
-    const PER_QUERY: usize = 5;
+    const PER_QUERY: usize = 10;
     /// Maximum number of times a request can retry before failing
     const MAX_ATTEMPTS: usize = 10;
-
-    let mut headers = HeaderMap::with_capacity(1);
-    headers.insert(
-        AUTHORIZATION,
-        HeaderValue::from_str(&format!("Bearer {}", token))?,
-    );
-
-    let client = Client::builder()
-        .user_agent(format!("Committer/{}", VERSION))
-        .default_headers(headers)
-        .build()
-        .map_err(SearchError::CreateClient)?;
 
     let mut users: Vec<User> = Vec::new();
     let mut last_cursor: Option<String> = None;
 
     let mut attempts = 0;
 
+    let mut min_followers = -1;
+
     while users.len() < USERS {
         let mut query = String::new();
 
-        for location in locations.include {
+        for location in location.include {
             query.push_str(" location:");
             query.push_str(location);
+        }
+
+        for location in location.exclude {
+            query.push_str(" -location:");
+            query.push_str(location);
+        }
+
+        if min_followers >= 0 {
+            query.push_str(" followers:<");
+            query.push_str(&min_followers.to_string());
         }
 
         query.push_str(" sort:followers-desc");
@@ -159,7 +225,7 @@ fn search_users(token: String, blacklist: &[Box<str>]) -> Result<Vec<User>, Sear
             after: last_cursor.take(),
         };
 
-        let res = match post_graphql_blocking::<UsersQuery, _>(&client, GRAPHQL_URL, variables) {
+        let res = match post_graphql_blocking::<UsersQuery, _>(client, GRAPHQL_URL, variables) {
             Ok(value) => value,
             Err(err) => {
                 attempts += 1;
@@ -258,12 +324,14 @@ fn search_users(token: String, blacklist: &[Box<str>]) -> Result<Vec<User>, Sear
                         .contributions_collection
                         .total_pull_request_contributions,
                 };
+                min_followers = user.followers;
 
                 users.push(user);
                 last_cursor = Some(cursor);
             });
-        println!("Progress: {}/{}", users.len(), USERS);
+
+        debug!("Progress: {}/{}", users.len(), USERS);
     }
 
-    Ok(users)
+    Ok((users, min_followers))
 }
